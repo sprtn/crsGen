61bdc942f68e9091ee8946b9a44c93dd40f3d198
sprtn <jompawars@gmail.com>
2017-06-03 00:28:16 +0200

    Derping with positioning a label. Fixed some code, created some basic methods and elements.

--- crsGen/MainWindow.xaml.cs
+++ crsGen/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,38 @@ namespace crsGen
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		private void GenerateCurseBtn_Click(object sender, RoutedEventArgs e)
+		{
+			if (CurseBox.Visibility != Visibility.Visible)
+				CurseBox.Visibility = Visibility.Visible;
+			CurseBox.Text = "Newly Generated Curse";
+		}
+
+		private void CurseBox_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			CopyCursetoClipboard();
+		}
+
+		private void CopyCursetoClipboard()
+		{
+			Clipboard.SetText(CurseBox.Text);
+			notificaitonLabel.Content = "Curse added to clipboard!";
+			notificaitonLabel.HorizontalAlignment = "";
+			notificationLabel.VerticalAlignment = "";
+			notificaitonLabel.Visibility = Visibility.Visible;
+		}
+
+		private async void notificationLabel_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+		{
+			await Task.Delay(3000);
+			notificaitonLabel.Visibility = Visibility.Hidden;
+		}
+
+		private void CopyBtn_Click(object sender, RoutedEventArgs e)
+		{
+			CopyCursetoClipboard();
 		}
 	}
 }
